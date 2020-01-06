116181e38fc2fa3fc14bd9bcc6272c645e96ff89
Nurlan Huseynov <nurlanah@code.edu.az>
2020-01-06 21:02:17 +0400

    fixed add delete and update user skills

--- DevForm/DevFormAz/DevFormAz/Controllers/HomeController.cs
+++ DevForm/DevFormAz/DevFormAz/Controllers/HomeController.cs
@@ -64,7 +64,7 @@ namespace DevFormAz.Controllers
         }
 
         [HttpPost]
-        public ActionResult UserPanel([Bind(Exclude="Image")]UserDetail userChanges,string firstname,string lastname,string email,HttpPostedFileBase image,string skills)
+        public ActionResult UserPanel([Bind(Exclude="Image")]UserDetail userChanges,string firstname,string lastname,string email,HttpPostedFileBase image,string skills,string checker)
         {
             var user = db.UserDetails.Find((int)Session["UserId"]);
             if (image != null && İmageControl.CheckImageType(image))
@@ -75,23 +75,49 @@ namespace DevFormAz.Controllers
                     user.Image = imgName;
                 }
             }
+
             
-            if(skills != null && skills != "")
-            {
                 var skillarr = skills.Split(' ');
 
-
+                var userCustomSkills = checker.Split(' ').ToList(); // Bununla evvelki bacariqlarin saxlayiriq
+                var checkUserSkill = user.Skills.Select(u => u.Name).ToArray(); // bununla dbda userin daxil edilen bacariginin olub olmamasin yoxlayiriq
 
                 for (var i = 0; i<skillarr.Length;i++)
                 {
-                    if (skillarr[i] != " " && db.Skills.Find((int)Session["UserId"]).Name != skillarr[i])
-                    db.Skills.Add(new Skill() { Name = skillarr[i],UserDetailId = user.Id });
+                    if(!checkUserSkill.Contains(skillarr[i]) && skillarr[i] != " " && skillarr[i] != "")
+                    {
+                        db.Skills.Add(new Skill() { Name = skillarr[i], UserDetailId = user.Id });
+                    }
+                    else
+                    {
+                        if(!userCustomSkills.Contains(null))
+                        userCustomSkills.Remove(skillarr[i]);
+                    }
                 }
-                
-            }
+
+                foreach(var item in userCustomSkills)
+                {
+                    var deletedSkill = user.Skills.Where(n => n.Name == item).FirstOrDefault();
+                    if (deletedSkill!=null)
+                    {
+                        db.Skills.Remove(deletedSkill);
+                    }
+                    
+                }
+            
             user.User.FirstName = firstname;
             user.User.Lastname = lastname;
-            user.User.Email = email;
+
+            if(!db.Users.Any(u => u.Email == email))
+            {
+                user.User.Email = email;
+            }
+            else
+            {
+                ViewBag.EmailErrorMsg = "Belə bir email artıq movcuddur";
+                return RedirectToAction("UserPanel","Home");
+            }
+            
             
             user.Biography = userChanges.Biography;
             user.Country = userChanges.Country;
@@ -100,7 +126,7 @@ namespace DevFormAz.Controllers
             user.LinkedinLink = userChanges.LinkedinLink;
 
             db.SaveChanges();
-            return RedirectToAction("ProfilePage","Home");
+            return RedirectToAction("UserPanel", "Home");
         }
 
 
